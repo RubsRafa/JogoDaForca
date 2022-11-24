b6415bf36241e1a338b1282a5394d195531d0b23
RubsRafa <rubiarafaela2000@gmail.com>
2022-11-23 21:48:21 -0300

    versao com layout incompleto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 
 function App() {
+  const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
   return (
-    <div className="App"> Hello
+    <div className="jogo">
+      <div className="visual">
+        <div className="forca">
+          <img src="./assets/forca0.png" />
+        </div>
+
+        <div className="conteudo">
+          <div className='palavra'>
+            <h1>Escolher palavra</h1>
+          </div>
+          <div className="tracos">
+            <h1 className="palavrasecreta"></h1>
+            <div className="traco"></div>
+          </div>
+        </div>
+      </div>
+      <div className="letras">
+        {alfabeto.map((letra) => <div className="letra">{letra.toUpperCase()}</div>)}
+      </div>
+      <div className="tentativas">
+        <h1>Já sei a palavra!</h1>
+        <input></input>
+        <div>Chutar</div>
+      </div>
     </div>
   );
 }
